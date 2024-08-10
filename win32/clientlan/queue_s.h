61261b956152f32f62ec30cd6aace76d4bf48f9f
imbakan <131257516+imbakan@users.noreply.github.com>
2024-08-10 11:18:20 +0800

    initial commit

---
+++ win32/clientlan/queue_s.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+typedef struct NODE_S NODE_S;
+
+struct NODE_S
+{
+	wchar_t* str;
+	NODE_S* Next;
+};
+
+class CQueue_s
+{
+private:
+	NODE_S* First, * Last;
+	int Count;
+
+public:
+	CQueue_s();
+	~CQueue_s();
+
+	int GetCount();
+	bool IsEmpty();
+	void Add(wchar_t* str);
+	void Remove(wchar_t* str, size_t n);
+
+	void GetFirstNode(wchar_t* str, size_t n);
+	void SetFirstNode(wchar_t* str);
+};
