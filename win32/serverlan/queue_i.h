0a97fef0c9a4b45010ae82740e369bcc2d55df15
imbakan <131257516+imbakan@users.noreply.github.com>
2024-08-10 11:11:39 +0800

    initial commit

---
+++ win32/serverlan/queue_i.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+typedef struct NODE_I NODE_I;
+
+struct NODE_I
+{
+	int value;
+	NODE_I* Next;
+};
+
+class CQueue_i
+{
+private:
+	NODE_I* First, * Last;
+	int Count;
+
+public:
+	CQueue_i();
+	~CQueue_i();
+
+	int GetCount();
+	bool IsEmpty();
+	void Add(int value);
+	void Return(int value);
+	void Remove(int* value);
+};
