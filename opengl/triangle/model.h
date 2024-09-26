4d73e614fe00f694d612bb0606ff83224700a44d
imbakan <131257516+imbakan@users.noreply.github.com>
2024-09-26 17:28:19 +0800

    initial commit

---
+++ opengl/triangle/model.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+class CModel
+{
+private:
+	int location;
+	int count;
+	GLuint vao;
+
+	void Generate(int vertex_coordinate, int color_coordinate, int* stride, float** vertices, int* size, int* count);
+
+public:
+	CModel();
+	~CModel();
+
+	void Create(GLuint handle);
+	void Destroy();
+	void Draw(float* matrix);
+};
