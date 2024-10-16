b7afd1010a1ed38baee924347b4a93f1bdced30e
imbakan <131257516+imbakan@users.noreply.github.com>
2024-10-16 19:23:32 +0800

    initial commit

--- win32/serverlan/client.h
+++ win32/serverlan/client.h
@@ -7,25 +7,24 @@ class CClient
 {
 private:
 
-    static const int MAX_PORT_STRING    = 10;
-    static const int MAX_IP_STRING      = 20;
-    static const int MAX_NAME_STRING    = 100;
+    static const int MAX_NAME_STRING = 100;
 
     static const int BUFFER_SIZE = 32768;
 
-    static const int NEED_DATA      = 3101;
-    static const int SET_ORDER      = 3102;
-    static const int GET_INTEGER    = 3103;
-    static const int GET_LONGLONG   = 3104;
-    static const int GET_STRING     = 3105;
-    static const int GET_CLIENT     = 3106;
+    static const int NEED_DATA = 3101;
+    static const int SET_ORDER = 3102;
+    static const int GET_INTEGER = 3103;
+    static const int GET_LONGLONG_1 = 3104;
+    static const int GET_LONGLONG_2 = 3105;
+    static const int GET_STRING = 3106;
+    static const int GET_CLIENT = 3107;
 
-    static const int STRINGS        = 3202;
+    static const int STRINGS = 3202;
 
-    static const int JOIN               = 3301;
-    static const int CLIENT_RUNNING     = 3303;
-    static const int FORWARD            = 3307;
-    static const int RESEND             = 3308;
+    static const int JOIN = 3301;
+    static const int CLIENT_RUNNING = 3303;
+    static const int FORWARD = 3307;
+    static const int RESEND = 3308;
 
     HANDLE thread;
     HWND hWnd, hList;
@@ -41,9 +40,9 @@ private:
     void OnNeedData(char* buffer, int* index, int* buffer_size, bool* needdata, CQueue_i* order, int* next);
     void OnSetOrder(bool* needdata, CQueue_i* order, int* next, int msg);
     void OnGetInteger(char* buffer, int* index, CQueue_i* order, int* next, int* value);
-    void OnGetLongLong(char* buffer, int* index, CQueue_i* order, int* next, long long* value, int* idx);
+    void OnGetLongLong(char* buffer, int* index, CQueue_i* order, int* next, long long* value, int datatype);
     void OnGetString(char* buffer, int* index, CQueue_i* order, int* next, int len, wchar_t** str);
-    void OnGetClient(CQueue_i* order, int* next, long long* llvalue, CClient** client);
+    void OnGetClient(CQueue_i* order, int* next, long long llvalue, CClient** client);
 
     void OnClientRunning(CQueue_i* order, int* next, wchar_t* str);
     void OnResend(char* buffer, int* index, int* buffer_size, bool* needdata, CQueue_i* order, int* next, CClient* client, long long* llvalue);
@@ -55,7 +54,7 @@ public:
     static const int ATTRIBUTES = 3201;
 
     static const int CONFIRM = 3304;
-    static const int LEAVE  = 3302;
+    static const int LEAVE = 3302;
 
     static const int REPLY_DEVICE = 3401;
     static const int REPLY_DEVICES = 3402;
