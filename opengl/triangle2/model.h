a412ab957b6307be69e677777fe15e346470eeab
imbakan <131257516+imbakan@users.noreply.github.com>
2025-12-18 10:52:43 +0800

    initial commit

---
+++ opengl/triangle2/model.h
@@ -0,0 +1,22 @@
+
+// GL_VERSION_1_5 GL_VERSION_2_0 GL_VERSION_3_0
+
+#pragma once
+
+class CModel
+{
+private:
+	int location;
+	int count;
+	GLuint vao;
+
+	void Generate(int vertex_size, int color_size, int* stride, float** vertices, int* size, int* count);
+
+public:
+	CModel();
+	~CModel();
+
+	void Create(GLuint handle);
+	void Destroy();
+	void Draw(float* matrix);
+};
