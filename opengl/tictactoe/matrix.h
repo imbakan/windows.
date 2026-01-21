bb64913c8acc2f7fbfdaac66e91fb22d551bdd0c
imbakan <131257516+imbakan@users.noreply.github.com>
2026-01-21 19:09:39 +0800

    initial commit

---
+++ opengl/tictactoe/matrix.h
@@ -0,0 +1,46 @@
+
+#pragma once
+
+const int NA = 16;
+const int NB = 4;
+
+union mat_type {
+    float a[NA];
+    float b[NB][NB];
+};
+
+class CMatrix
+{
+private:
+    mat_type m;
+
+public:
+    CMatrix();
+    CMatrix(float a11, float a12, float a13, float a14,
+        float a21, float a22, float a23, float a24,
+        float a31, float a32, float a33, float a34,
+        float a41, float a42, float a43, float a44);
+    CMatrix(float* a);
+    ~CMatrix();
+
+    void Output();
+
+    void LookAt(float eyex, float eyey, float eyez, float centerx, float centery, float centerz, float upx, float upy, float upz);
+
+    void Orthographic(float left, float right, float bottom, float top, float znear, float zfar);
+    void Perspective(float left, float right, float bottom, float top, float znear, float zfar);
+    void Perspective(float fovy, float aspect, float znear, float zfar);
+
+    void Identity();
+    void Translate(float x, float y, float z);
+    void Rotate(float a, float rx, float ry, float rz);
+
+    void operator =(CMatrix m);
+
+    CMatrix operator*(CMatrix m);
+
+    void operator *=(CMatrix m);
+
+    operator float* ();
+};
+
