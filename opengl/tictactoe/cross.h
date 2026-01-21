9380b1bb16c47691ae36814273f7ec6657e3a320
imbakan <131257516+imbakan@users.noreply.github.com>
2026-01-21 19:05:08 +0800

    initial commit

---
+++ opengl/tictactoe/cross.h
@@ -0,0 +1,22 @@
+
+// GL_VERSION_1_5 GL_VERSION_2_0 GL_VERSION_3_0
+
+#pragma once
+
+class CCross
+{
+private:
+	int location1, location2;
+	int count;
+	GLuint vao;
+
+	void Generate(int vertex_size, int* stride, float** vertices, int* size, int* count);
+
+public:
+	CCross();
+	~CCross();
+
+	void Create(GLuint handle);
+	void Destroy();
+	void Draw(float* matrix);
+};
