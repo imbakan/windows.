0fd19c7598a9104e70e4136c40ee4e8205779dfb
imbakan <131257516+imbakan@users.noreply.github.com>
2026-02-04 19:34:32 +0800

    initial commit

---
+++ win32/screenrecorder/framework.h
@@ -0,0 +1,35 @@
+// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <time.h> 
+
+// media foundation
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfreadwrite.h>
+#include <mferror.h>
+
+#include <d3d9.h>
+
+/*
+mfreadwrite.lib
+mfplat.lib
+mfuuid.lib
+d3d9.lib
+*/
