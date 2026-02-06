6535bd44bc175128cb55c5bdf6ae0bb36cd98d26
imbakan <131257516+imbakan@users.noreply.github.com>
2026-02-06 19:20:15 +0800

    initial commit

---
+++ opengl/bezier/Resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by bezier.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_BEZIER_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_BEZIER			107
+#define IDI_SMALL				108
+#define IDC_BEZIER			109
+
+#define IDD_DIALOG1				110
+
+#define IDM_OPEN				111
+#define IDM_SAVE_AS				112
+
+#define IDM_ADD				121
+#define IDM_EDIT			122
+#define IDM_DELETE			123
+
+#define IDM_CONTROL_POINT	131
+
+#define IDC_TEXT1               1001
+#define IDC_SCROLLBAR1			1002
+
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
