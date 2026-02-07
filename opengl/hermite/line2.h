5c54fb02b01f95c1eb96de5b6e6edf4615e074b3
imbakan <131257516+imbakan@users.noreply.github.com>
2026-02-07 18:29:32 +0800

    initial commit

---
+++ opengl/hermite/line2.h
@@ -0,0 +1,25 @@
+
+// GL_VERSION_1_5 GL_VERSION_2_0 GL_VERSION_3_0
+
+#pragma once
+
+class CLine2
+{
+private:
+	int location1, location2;
+	float color[4];
+	int count, vertex_size;
+	GLuint vao, vbo;
+
+public:
+	CLine2();
+	~CLine2();
+
+	void Create(GLuint handle, float* color);
+	void Destroy();
+	void Draw(float* matrix);
+
+	void Update(float* vertices, int count, int size);
+
+	void Verify();
+};
