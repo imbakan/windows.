64cd80d97f99e1bbc50a345ae586b926e7a845d3
imbakan <131257516+imbakan@users.noreply.github.com>
2026-02-11 18:34:35 +0800

    initial commit

---
+++ opengl/texture2/model.h
@@ -0,0 +1,25 @@
+
+// GL_VERSION_1_5 GL_VERSION_2_0 GL_VERSION_3_0
+
+#pragma once
+
+class CModel
+{
+private:
+	int location1, location2;
+	int count;
+	GLuint vao;
+	float opacity;
+
+	void Generate(int vertex_size, int texture_size, int* stride, float** vertices, int* size, int* count, float width, float height, float iwidth);
+
+public:
+	CModel();
+	~CModel();
+
+	void Create(GLuint handle, float width, float height, float iwidth);
+	void Destroy();
+	void Draw(float* matrix);
+
+	void SetOpacity(float opacity);
+};
