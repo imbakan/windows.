7eb8bcf22a9ae4c7f244c53b5efb143a5fc8f8db
imbakan <131257516+imbakan@users.noreply.github.com>
2026-06-30 09:00:57 +0800

    initial commit

--- win32/clientbt/framework.h
+++ win32/clientbt/framework.h
@@ -8,10 +8,15 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
-#define WM_LOG_MESSAGE		WM_USER + 5
+#define WM_MESSAGE			WM_USER + 5
 #define WM_CONNECTING		WM_USER + 6
 #define WM_RUNNING			WM_USER + 7
 #define WM_SHUTTING_DOWN	WM_USER + 8
+
+#define WM_POPULATE_WITH_DEVICE	WM_USER + 9
+#define WM_POPULATE_WITH_DRIVE	WM_USER + 10
+#define WM_POPULATE_WITH_FOLDER	WM_USER + 11
+#define WM_DEPOPULATE_BY_DEVICE	WM_USER + 12
 
 // Windows Header Files
 #include <windows.h>
@@ -28,7 +33,7 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
-#include <time.h> 
+#include <time.h>
 
 wchar_t* GetErrorMessage(int err);
 
