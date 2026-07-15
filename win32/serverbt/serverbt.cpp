13c7f782594fa6abc1c36ef669079ec8541c886b
imbakan <131257516+imbakan@users.noreply.github.com>
2026-07-15 08:07:03 +0800

    initial commit

--- win32/serverbt/serverbt.cpp
+++ win32/serverbt/serverbt.cpp
@@ -1,24 +1,21 @@
 
+// Bluetooth - server side
+
 #include "framework.h"
-#include "server_bt.h"
+#include "bt_server.h"
 #include "server.h"
 #include "client.h"
 
 const int MAX_LOADSTRING = 100;
-
 const int PADDING = 2;
-const int SPLITTER = 6;
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // main window class name
 
-RECT rect;
-bool dragging;
-
 HIMAGELIST himl;
-HWND hList1, hEdit1;
+HWND hList1;
 
 WSADATA wsadata;
 CServer server;
@@ -26,26 +23,22 @@ CServer server;
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 
-void OnLogMessage(HWND hWnd, WPARAM wParam, LPARAM lParam);
+void OnAfterWindowDisplayed(HWND hWnd, WPARAM wParam, LPARAM lParam);
+
+void OnMessage(HWND hWnd, WPARAM wParam, LPARAM lParam);
 void OnServerRunning(HWND hWnd, WPARAM wParam, LPARAM lParam);
 void OnServerShuttingDown(HWND hWnd, WPARAM wParam, LPARAM lParam);
 void OnCreateClient(HWND hWnd, WPARAM wParam, LPARAM lParam);
 void OnClientRunning(HWND hWnd, WPARAM wParam, LPARAM lParam);
 void OnClientShuttingDown(HWND hWnd, WPARAM wParam, LPARAM lParam);
 
-void OnItemChanged(HWND hWnd, LPARAM lParam);
-void OnSetFocus(HWND hWnd, LPARAM lParam);
-void OnKillFocus(HWND hWnd, LPARAM lParam);
-
-void OnLButtonDown(HWND hWnd, int x, int y);
-void OnLButtonUp(HWND hWnd, int x, int y);
-void OnMouseMove(HWND hWnd, int x, int y);
 void OnSize(HWND hWnd, int width, int height);
 
 void OnPaint(HWND hWnd);
 void OnCreate(HWND hWnd);
 void OnDestroy(HWND hWnd);
-void OnClose(HWND hWnd);
+
+void OnItemChanged(HWND hWnd, LPARAM lParam);
 
 void OnServerRun(HWND hWnd);
 void OnServerShutdown(HWND hWnd);
@@ -60,7 +53,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
     // Initialize global strings.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_SERVERBT, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_BTSERVER, szWindowClass, MAX_LOADSTRING);
 
     // Registers the window class.
     WNDCLASSEXW wcex;
@@ -72,10 +65,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SERVERBT));
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BTSERVER));
     wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_SERVERBT);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_BTSERVER);
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
@@ -109,7 +102,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SERVERBT));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_BTSERVER));
 
     MSG msg;
 
@@ -131,31 +124,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
-    case WM_LOG_MESSAGE:            OnLogMessage(hWnd, wParam, lParam);         break;
+    case WM_AFTERWINDOWDISPLAYED: OnAfterWindowDisplayed(hWnd, wParam, lParam);  break;
+
+    case WM_MESSAGE:                OnMessage(hWnd, wParam, lParam);            break;
     case WM_SERVER_RUNNING:         OnServerRunning(hWnd, wParam, lParam);      break;
     case WM_SERVER_SHUTTING_DOWN:   OnServerShuttingDown(hWnd, wParam, lParam); break;
     case WM_CREATE_CLIENT:          OnCreateClient(hWnd, wParam, lParam);       break;
     case WM_CLIENT_RUNNING:         OnClientRunning(hWnd, wParam, lParam);      break;
     case WM_CLIENT_SHUTTING_DOWN:   OnClientShuttingDown(hWnd, wParam, lParam); break;
-
-    case WM_NOTIFY:
-        if (((LPNMHDR)lParam)->idFrom == IDC_LIST1) {
-            switch (((LPNMHDR)lParam)->code) {
-            case LVN_ITEMCHANGED:   OnItemChanged(hWnd, lParam);    break;
-            case NM_SETFOCUS:       OnSetFocus(hWnd, lParam);       break;
-            case NM_KILLFOCUS:      OnKillFocus(hWnd, lParam);      break;
-            }
-        }
-        else {
-            return DefWindowProc(hWnd, message, wParam, lParam);
-        }
-
-        break;
-
-    case WM_LBUTTONDOWN:    OnLButtonDown(hWnd, LOWORD(lParam), HIWORD(lParam)); break;
-    case WM_LBUTTONUP:	    OnLButtonUp(hWnd, LOWORD(lParam), HIWORD(lParam));	break;
-    case WM_MOUSEMOVE:	    OnMouseMove(hWnd, LOWORD(lParam), HIWORD(lParam));	break;
-    case WM_SIZE:           OnSize(hWnd, LOWORD(lParam), HIWORD(lParam)); break;
 
     case WM_COMMAND:
 
@@ -170,50 +146,46 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
 
+    case WM_SIZE:           OnSize(hWnd, LOWORD(lParam), HIWORD(lParam)); break;
     case WM_PAINT:          OnPaint(hWnd);                                          break;
     case WM_CREATE:         OnCreate(hWnd);                                         break;
     case WM_DESTROY:        OnDestroy(hWnd);                                        break;
-    case WM_CLOSE:          OnClose(hWnd);								            break;
+
+    case WM_NOTIFY:
+
+        if (((LPNMHDR)lParam)->idFrom == IDC_LIST1) {
+
+            if (((LPNMHDR)lParam)->code == LVN_ITEMCHANGED) {
+                OnItemChanged(hWnd, lParam);
+            }
+            else {
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+
+        }
+        else {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+
+        break;
+
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
-void OnLogMessage(HWND hWnd, WPARAM wParam, LPARAM lParam)
+void OnAfterWindowDisplayed(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
-    wchar_t* str2 = (wchar_t*)lParam;
+    OnServerRun(hWnd);
+}
 
-    wchar_t* str, * str1;
-    int n, n1, n2;
+void OnMessage(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    wchar_t* str = (wchar_t*)lParam;
 
-    n1 = GetWindowTextLength(hEdit1);
-
-    if (n1 == 0) {
-        SetWindowText(hEdit1, str2);
-        return;
-    }
-
-    n2 = (int)wcslen(str2);
-    n = n1 + n2 + 3;
-
-    str1 = new wchar_t[n1 + 1];
-    str = new wchar_t[n];
-
-    GetWindowText(hEdit1, str1, n1 + 1);
-
-    wcscpy_s(str, n, str1);
-    wcscat_s(str, n, L"\r\n");
-    wcscat_s(str, n, str2);
-
-    SetWindowText(hEdit1, str);
-
-    delete[] str1;
-    delete[] str;
-
-    SendMessage(hEdit1, EM_SETSEL, 0, -1);
-    SendMessage(hEdit1, EM_SETSEL, -1, -1);
-    SendMessage(hEdit1, EM_SCROLLCARET, 0, 0);
+    OutputDebugString(str);
+    OutputDebugString(L"\n");
 }
 
 void OnServerRunning(HWND hWnd, WPARAM wParam, LPARAM lParam)
@@ -228,9 +200,9 @@ void OnServerRunning(HWND hWnd, WPARAM wParam, LPARAM lParam)
     EnableMenuItem(hMenu, IDM_SHUTDOWN, MF_ENABLED);
     EnableMenuItem(hMenu, IDM_CLOSE, MF_DISABLED);
     EnableMenuItem(hMenu, IDM_EXIT, MF_DISABLED);
+
 }
 
-//
 void OnServerShuttingDown(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
     wchar_t str[MAX_LOADSTRING];
@@ -256,61 +228,22 @@ void OnCreateClient(HWND hWnd, WPARAM wParam, LPARAM lParam)
 void OnClientRunning(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
     CClient* client = (CClient*)lParam;
-    CClient* client1;
+    CClient* item;
     LV_ITEM lvi;
-    struct tm Tm;
-    time_t lltime;
+    time_t ltime;
+    struct tm ttime;
     int index, i, n;
-    wchar_t str1[100], str2[50];
+    wchar_t str1[100], str2[50], str3[50], str4[50];
 
-    // ang server ay nag-aasign ng id sa kumonektang client
-    // isend 'to sa client
-    client->Send(CClient::CONNECTED);
-    client->Send(client->GetId());
-
-    n = ListView_GetItemCount(hList1);
-
-    if (n > 0) {
-
-        // isend ang mga pangalan ng client na nasa list view sa kumonektang client
-        lvi.mask = LVIF_PARAM;
-
-        for (i = 0; i < n; i++) {
-
-            lvi.iItem = i;
-            ListView_GetItem(hList1, &lvi);
-
-            client1 = (CClient*)lvi.lParam;
-
-            client->Send(CClient::ATTRIBUTES);
-            client->Send(client1->GetName());
-            client->Send(client1->GetId());
-        }
-
-        client->Send(CClient::REPLY_DEVICE);
-
-        // isend ang pangalan ng kumonektang client sa mga client na nasa list view
-        for (i = 0; i < n; i++) {
-
-            lvi.iItem = i;
-            ListView_GetItem(hList1, &lvi);
-
-            client1 = (CClient*)lvi.lParam;
-
-            client1->Send(CClient::ATTRIBUTES);
-            client1->Send(client->GetName());
-            client1->Send(client->GetId());
-            client1->Send(CClient::REPLY_DEVICE);
-        }
-    }
-
-    // iadd ang kumonektang client sa list view
     wcscpy_s(str1, 100, client->GetName());
 
-    time(&lltime);
-    _localtime64_s(&Tm, &lltime);
-    swprintf_s(str2, 50, L"%2d-%02d-%04d %2d:%02d:%02d", Tm.tm_mon + 1, Tm.tm_mday, Tm.tm_year + 1900, Tm.tm_hour, Tm.tm_min, Tm.tm_sec);
+    time(&ltime);
+    _localtime64_s(&ttime, &ltime);
+    wcsftime(str2, 50, L"%b %#d, %Y", &ttime);
+    wcsftime(str3, 50, L"%r", &ttime);
+    swprintf_s(str4, 50, L"%s %s", str2, str3);
 
+    // idagdag ang bagong client sa list view
     index = ListView_GetItemCount(hList1);
 
     lvi.mask = LVIF_TEXT | LVIF_STATE | LVIF_PARAM;
@@ -322,23 +255,59 @@ void OnClientRunning(HWND hWnd, WPARAM wParam, LPARAM lParam)
     lvi.lParam = (LPARAM)client;
 
     ListView_InsertItem(hList1, &lvi);
-    ListView_SetItemText(hList1, index, 1, str2);
+    ListView_SetItemText(hList1, index, 1, str4);
+ 
+    n = ListView_GetItemCount(hList1);
 
+    // isend ang mga nasa list view sa kumonektang client
+    lvi.mask = LVIF_PARAM;
+
+    for (i = 0; i < n; i++) {
+
+        lvi.iItem = i;
+        ListView_GetItem(hList1, &lvi);
+
+        item = (CClient*)lvi.lParam;
+
+        client->Send(CClient::ATTRIBUTE);
+        client->Send(item->GetId());
+        client->Send(item->GetName());
+    }
+
+    client->Send(CClient::DEVICE);
+
+    // isend ang kumonektang client sa mga nasa list view
+    lvi.mask = LVIF_PARAM;
+
+    for (i = 0; i < n; i++) {
+
+        lvi.iItem = i;
+        ListView_GetItem(hList1, &lvi);
+
+        item = (CClient*)lvi.lParam;
+
+        if (item->GetId() == client->GetId()) continue;
+
+        item->Send(CClient::ATTRIBUTE);
+        item->Send(client->GetId());
+        item->Send(client->GetName());
+
+        item->Send(CClient::DEVICE);
+    }
 }
 
 void OnClientShuttingDown(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
-    CClient* client1 = (CClient*)lParam;
-    CClient* client2;
+    CClient* client = (CClient*)lParam;
+    CClient* item;
     LVFINDINFO lvfi;
     LV_ITEM lvi;
     int i, n;
-    //wchar_t str[100];
 
-    // iremove ang client na 'to sa list view
+    // alisin ang client nito sa list view
     lvfi.flags = LVFI_PARAM;
     lvfi.psz = NULL;
-    lvfi.lParam = (LPARAM)client1;
+    lvfi.lParam = (LPARAM)client;
     lvfi.pt.x = 0;
     lvfi.pt.y = 0;
     lvfi.vkDirection = 0;
@@ -346,138 +315,27 @@ void OnClientShuttingDown(HWND hWnd, WPARAM wParam, LPARAM lParam)
     i = ListView_FindItem(hList1, -1, &lvfi);
     ListView_DeleteItem(hList1, i);
 
-    if (ListView_GetItemCount(hList1) == 0) {
-        HMENU hMenu = GetMenu(hWnd);
-        EnableMenuItem(hMenu, IDM_CLOSE, MF_BYCOMMAND | MF_DISABLED);
-    }
-
-    // ipaalam sa mga client na nasa list view na wala na ang client na 'to
+    // isend ang nagleave na client sa mga nasa list view para iremove
     n = ListView_GetItemCount(hList1);
 
-    //swprintf_s(str, 100, L"%10d\n", n);
-    //OutputDebugString(str);
+    lvi.mask = LVIF_PARAM;
 
     for (i = 0; i < n; i++) {
 
         lvi.iItem = i;
         ListView_GetItem(hList1, &lvi);
 
-        client2 = (CClient*)lvi.lParam;
+        item = (CClient*)lvi.lParam;
 
-        //swprintf_s(str, 100, L"%-20s%20lld\n", client2->GetName(), client2->GetId());
-        //OutputDebugString(str);
-
-        client2->Send(CClient::LEAVE);
-        client2->Send(client1->GetId());
+        item->Send(CClient::LEAVE);
+        item->Send(client->GetId());
     }
 
-    delete client1;
 }
-
-void OnItemChanged(HWND hWnd, LPARAM lParam)
-{
-    HWND hList = ((LPNMHDR)lParam)->hwndFrom;
-    LPNMLISTVIEW pnmv = (LPNMLISTVIEW)lParam;
-
-    UINT uEnabled = (pnmv->uNewState == (LVIS_FOCUSED | LVIS_SELECTED)) ? MF_ENABLED : MF_DISABLED;
-
-    HMENU hMenu = GetMenu(hWnd);
-    EnableMenuItem(hMenu, IDM_CLOSE, uEnabled);
-}
-
-void OnSetFocus(HWND hWnd, LPARAM lParam)
-{
-    HMENU hMenu = GetMenu(hWnd);
-    EnableMenuItem(hMenu, IDM_CLOSE, MF_DISABLED);
-}
-
-void OnKillFocus(HWND hWnd, LPARAM lParam)
-{
-    HMENU hMenu = GetMenu(hWnd);
-    EnableMenuItem(hMenu, IDM_CLOSE, MF_DISABLED);
-}
-
-
-void OnLButtonDown(HWND hWnd, int x, int y)
-{
-    if (rect.left < x && x < rect.right && rect.top < y && y < rect.bottom) {
-        dragging = true;
-        SetCursor(LoadCursor(NULL, IDC_SIZENS));
-        SetCapture(hWnd);
-    }
-}
-
-void OnLButtonUp(HWND hWnd, int x, int y)
-{
-    dragging = false;
-    ReleaseCapture();
-}
-
-void OnMouseMove(HWND hWnd, int x, int y)
-{
-    RECT cr;
-    int x1, y1, y2, width1, height1, height2;
-
-    if (dragging) {
-
-        GetClientRect(hWnd, &cr);
-        if (y < 100 || (cr.bottom - 100) < y) return;
-
-        rect.top = y;
-        rect.bottom = rect.top + SPLITTER;
-
-        x1 = y1 = PADDING;
-        y2 = rect.bottom;
-
-        width1 = rect.right - rect.left;
-        height1 = rect.top - y1;
-        height2 = cr.bottom - rect.bottom - PADDING;
-
-        MoveWindow(hList1, x1, y1, width1, height1, TRUE);
-        MoveWindow(hEdit1, x1, y2, width1, height2, TRUE);
-
-    }
-    else {
-
-        if (rect.left < x && x < rect.right && rect.top < y && y < rect.bottom)
-            SetCursor(LoadCursor(NULL, IDC_SIZENS));
-    }
-}
-
-//          x1
-//       y1 +-------------------------------------+  ---      ---
-//          |                                     |   ^        ^
-//          |                                     |   |        |
-//          |                                     | height1    |
-//          |                                     |   |        |
-//          |L                                   R|   v        |
-//          +-------------------------------------+T ---       |
-//          |                                     |          height
-//       y2 +-------------------------------------+B ---       |
-//          |                                     |   ^        |
-//          |                                     |   |        |
-//          |                                     | height2    |
-//          |                                     |   |        |
-//          |                                     |   v        v
-//          +--------------width1-----------------+  ---      ---
-// 
-//          |<----------------width-------------->|
 
 void OnSize(HWND hWnd, int width, int height)
 {
-    rect.right = width - rect.left;
-
-    int x1, y1, y2, width1, height1, height2;
-
-    x1 = y1 = PADDING;
-    y2 = rect.bottom;
-
-    width1 = rect.right - rect.left;
-    height1 = rect.top - y1;
-    height2 = height - rect.bottom - PADDING;
-
-    MoveWindow(hList1, x1, y1, width1, height1, FALSE);
-    MoveWindow(hEdit1, x1, y2, width1, height2, FALSE);
+    MoveWindow(hList1, PADDING, PADDING, width - 2 * PADDING, height - 2 * PADDING, FALSE);
 }
 
 void OnPaint(HWND hWnd)
@@ -501,10 +359,6 @@ void OnCreate(HWND hWnd)
     EnableMenuItem(hMenu, IDM_SHUTDOWN, MF_DISABLED);
     EnableMenuItem(hMenu, IDM_CLOSE, MF_DISABLED);
     EnableMenuItem(hMenu, IDM_EXIT, MF_ENABLED);
-
-    rect.left = PADDING;
-    rect.top = 175;
-    rect.bottom = rect.top + SPLITTER;
 
     HICON hicon;
 
@@ -544,21 +398,13 @@ void OnCreate(HWND hWnd)
     wcscpy_s(str, 100, L"Date");
     if (ListView_InsertColumn(hList1, 1, &lvc) == -1) return;
 
-    // gumawa ng font
-    HFONT hFont = CreateFont(16, 0, 0, 0, FW_REGULAR, FALSE, FALSE, 0, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_MODERN, L"Consolas");
-
-    // gumawa ng edit control
-    hEdit1 = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_READONLY | WS_BORDER,
-        0, 0, 1, 1, hWnd, (HMENU)IDC_EDIT1, hInst, NULL);
-
-    // gamitin ang font na 'to
-    SendMessage(hEdit1, WM_SETFONT, (WPARAM)hFont, 0);
-
     // initialize ang winsock
     int errcode = WSAStartup(MAKEWORD(2, 2), &wsadata);
 
     if (errcode != 0)
         OutputDebugString(GetErrorMessage(WSAGetLastError()));
+
+    PostMessage(hWnd, WM_AFTERWINDOWDISPLAYED, 0, (LPARAM)0);
 }
 
 void OnDestroy(HWND hWnd)
@@ -568,9 +414,15 @@ void OnDestroy(HWND hWnd)
     PostQuitMessage(0);
 }
 
-void OnClose(HWND hWnd)
+void OnItemChanged(HWND hWnd, LPARAM lParam)
 {
-    MessageBox(hWnd, L"Gamitin ang Exit menu.", L"Paalala", MB_ICONINFORMATION);
+    HWND hList = ((LPNMHDR)lParam)->hwndFrom;
+    LPNMLISTVIEW pnmv = (LPNMLISTVIEW)lParam;
+
+    UINT uEnabled = (pnmv->uNewState == (LVIS_FOCUSED | LVIS_SELECTED)) ? MF_ENABLED : MF_DISABLED;
+
+    HMENU hMenu = GetMenu(hWnd);
+    EnableMenuItem(hMenu, IDM_CLOSE, uEnabled);
 }
 
 void OnServerRun(HWND hWnd)
@@ -594,8 +446,6 @@ void OnClientClose(HWND hWnd)
     int i;
 
     i = ListView_GetNextItem(hList1, -1, LVNI_SELECTED);
-
-    if (i == -1) return;
 
     ZeroMemory(&lvi, sizeof(LV_ITEM));
 
