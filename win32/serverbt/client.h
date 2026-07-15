caa88f0b96db5a438e410a0db8fe9bf4de9b695b
imbakan <131257516+imbakan@users.noreply.github.com>
2026-07-15 08:02:35 +0800

    initial commit

--- win32/serverbt/client.h
+++ win32/serverbt/client.h
@@ -1,76 +1,36 @@
 
-#include "queue_i.h"
+#include "linkedlist.h"
 
 #pragma once
 
 class CClient
 {
 private:
-    static const int MAX_NAME_STRING    = 100;
 
+    static const int MAX_NAME_STRING = 100;
     static const int BUFFER_SIZE = 32768;
-    
+
     static const int NEED_DATA      = 3001;
     static const int SET_ORDER      = 3002;
     static const int GET_INTEGER    = 3003;
     static const int GET_LONGLONG   = 3004;
     static const int GET_STRING     = 3005;
+    static const int GET_ATTRIBUTE  = 3006;
 
-    //static const int ATTRIBUTES         = 4001;
-    static const int STRINGS            = 4002;
-    static const int COLLECT_STRING     = 4003;
-    static const int COLLECT_ATTRIBUTE  = 4004;
-    
-    static const int FIRST_RUN      = 5001;
-    //static const int CONNECTED        = 5002;
-    static const int JOIN           = 5003;
-    //static const int LEAVE          = 5004;
-    static const int FORWARD        = 5005;
-    static const int RESEND         = 5006;
-    static const int RUNNING        = 5007;
-
-    static const int REQUEST_DRIVE      = 6001;
-    static const int REQUEST_DIRECTORY  = 6002;
-    static const int REQUEST_FILE       = 6003;
-    static const int REQUEST_CONTENT    = 6004;
-    
-    //static const int REPLY_DEVICE       = 7001;
-    static const int REPLY_DRIVE        = 7002;
-    static const int REPLY_DIRECTORY    = 7003;
-    static const int REPLY_FILE         = 7004;
-    static const int REPLY_CONTENT_1    = 7005;
-    static const int REPLY_CONTENT_2    = 7006;
-    static const int REPLY_CONTENT_3    = 7007;
-
-    static const int ENUMERATE_DRIVE        = 8001;
-    static const int ENUMERATE_DIRECTORY    = 8002;
-    static const int ENUMERATE_FILES        = 8003;
-    static const int ENUMERATE_CONTENTS     = 8004;
-
-    static const int DEPOPULATE_BY_DEVICE       = 9001;
-    static const int POPULATE_WITH_DEVICE       = 9002;
-    static const int POPULATE_WITH_DRIVE        = 9003;
-    static const int POPULATE_WITH_DIRECTORY    = 9004;
-    static const int POPULATE_WITH_FILE         = 9005;
-    static const int POPULATE_WITH_CONTENT_1    = 9006;
-    static const int POPULATE_WITH_CONTENT_2    = 9007;
-    static const int POPULATE_WITH_CONTENT_3    = 9008;
-
-    void OnSetOrder(bool* needdata, CQueue_i* order, int* next, int cmd);
-    void OnNeedData(char* buffer, int* index, int* buffer_size, bool* needdata, CQueue_i* order, int* next);
-    void OnGetInteger(char* buffer, int* index, CQueue_i* order, int* next, int* value);
-    void OnGetLongLong(char* buffer, int* index, CQueue_i* order, int* next, long long* value);
-    void OnGetString(char* buffer, int* index, CQueue_i* order, int* next, int len, wchar_t** str);
-
-    void OnRunning(CQueue_i* order, int* next, wchar_t* str);
-
-    static DWORD WINAPI Function(LPVOID lpParam);
+    static const int JOIN       = 5003;
+    static const int CLIENT_ID  = 5004;
 
 public:
-    static const int ATTRIBUTES = 4001;
-    static const int CONNECTED = 5002;
-    static const int LEAVE = 5004;
-    static const int REPLY_DEVICE = 7001;
+    static const int LEAVE      = 5005;
+
+    static const int ATTRIBUTE  = 6001;
+    static const int DEVICE     = 6002;
+
+private:
+    static const int FORWARD        = 7001;
+    static const int SEND_ATTRIBUTE = 7003;
+
+    static const int RUNNING = 8001;
 
     HANDLE thread;
     HWND hWnd, hList;
@@ -78,8 +38,27 @@ public:
     wchar_t name[MAX_NAME_STRING];
     long long id;
 
-    CClient(HWND hWnd, HWND hList, SOCKET socket);
-    ~CClient();
+    void OnSetOrder(bool* needdata, CLinkedList<int>* order, int* next, int cmd);
+    void OnNeedData(char* buffer, int* index, int* buffer_size, bool* needdata, CLinkedList<int>* order, int* next);
+    void OnGetInteger(char* buffer, int* index, CLinkedList<int>* order, int* next, int* value);
+    void OnGetLongLong(char* buffer, int* index, CLinkedList<int>* order, int* next, CLinkedList<long long>* arrayll);
+    void OnGetString(char* buffer, int* index, CLinkedList<int>* order, int* next, int len, wchar_t** str);
+    void OnGetAttribute(CLinkedList<int>* order, int* next, CLinkedList <long long>* arrayll, long long* size, CClient** client);
+    void OnRunning(CLinkedList<int>* order, int* next, wchar_t* str);
+    void OnForward(char* buffer, int* index, int* buffer_size, bool* needdata, CLinkedList<int>* order, int* next, CClient* client, long long* data_size);
+    void OnSendAttribute(CLinkedList<int>* order, int* next, CClient* client);
+
+    void PrintOutput(int cmd);
+
+	static DWORD WINAPI Function(LPVOID lpParam);
+
+public:
+
+	CClient(HWND hWnd, HWND hList, SOCKET socket);
+	~CClient();
+
+    wchar_t* GetName();
+    long long GetId();
 
     void Run();
     void Shutdown();
@@ -87,8 +66,6 @@ public:
     void Send(int value);
     void Send(long long value);
     void Send(wchar_t* str);
-
-    wchar_t* GetName();
-    long long GetId();
+    void Send(char* value, int size);
 };
 
