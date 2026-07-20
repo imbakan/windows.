97e95700572911850510b64caefb5f911030cb7a
imbakan <131257516+imbakan@users.noreply.github.com>
2026-07-20 08:35:19 +0800

    initial commit

---
+++ console/eqn/queue.cpp
@@ -0,0 +1,146 @@
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "queue.h"
+
+CQueue::CQueue()
+{
+	First = Last = NULL;
+	Count = 0;
+}
+
+CQueue::~CQueue()
+{
+	Clear();
+}
+
+void CQueue::CapString(char* str)
+{
+	size_t i, n;
+
+	n = strlen(str);
+
+	for (i = 0; i < n; i++)
+		str[i] = toupper(str[i]);
+}
+
+int CQueue::GetCount()
+{
+	return Count;
+}
+
+bool CQueue::IsEmpty()
+{
+	return (Count == 0);
+}
+
+//                                                                       add
+//                                                               <---------------
+//      First                                             Last                     Node
+//     +----+    +----+    +----+    +----+    +----+    +----+                   +----+
+//     |    |--->|    |--->|    |--->|    |--->|    |--->|    |---> NULL          |    |---> NULL
+//     +----+    +----+    +----+    +----+    +----+    +----+                   +----+
+//
+void CQueue::Add(char* str, Token token)
+{
+	NODE* Node;
+	size_t n;
+
+	Node = new NODE;
+
+	Node->token = token;
+
+	n = strlen(str) + 1;
+	Node->str = new char[n];
+	strcpy_s(Node->str, n, str);
+
+	Node->Next = NULL;
+
+	if (IsEmpty())
+	{
+		First = Last = Node;
+	}
+	else
+	{
+		Last->Next = Node;
+		Last = Node;
+	}
+
+	Count++;
+}
+
+//               remove
+//         <-----------------
+//      First                                                                 Last
+//     +----+              +----+    +----+    +----+    +----+    +----+    +----+
+//     |    |--->          |    |--->|    |--->|    |--->|    |--->|    |--->|    |---> NULL
+//     +----+              +----+    +----+    +----+    +----+    +----+    +----+
+//
+void CQueue::Remove(char* str, int n)
+{
+	NODE* Node;
+
+	Node = First;
+	strcpy_s(str, n, Node->str);
+
+	First = First->Next;
+	Count--;
+
+	if (IsEmpty())
+		Last = First;
+
+	delete[] Node->str;
+	delete Node;
+}
+
+void CQueue::Reset()
+{
+	Pointer = First;
+}
+
+bool CQueue::Read(NODE** Node)
+{
+	if (Pointer == NULL) return false;
+
+	*Node = Pointer;
+
+	Pointer = Pointer->Next;
+
+	return true;
+}
+
+bool CQueue::Find(char* str)
+{
+	NODE* Node;
+	char str1[100], str2[100];
+	bool result;
+
+	strcpy_s(str2, 100, str);
+	CapString(str2);
+
+	result = false;
+
+	Reset();
+
+	while (Read(&Node)) {
+
+		strcpy_s(str1, 100, Node->str);
+		CapString(str1);
+
+		if (strcmp(str1, str2) == 0) {
+			result = true;
+			break;
+		}
+	}
+
+	return result;
+}
+
+void CQueue::Clear()
+{
+	char str[100];
+
+	while (!IsEmpty())
+		Remove(str, 100);
+}
